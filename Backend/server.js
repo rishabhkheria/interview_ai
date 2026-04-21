c052adb6d16f4a120c5c20074949907ccc158193
Rishabh Kheria <rishabhkheria231@gmail.com>
2026-04-21 18:45:57 +0530

    fix: code configuration updates for production deployment

--- Backend/server.js
+++ Backend/server.js
@@ -10,6 +10,7 @@ connectToDb();
 // generateInterviewReport({ resume, selfDescription, jobDescription });
 // invokeGeminiAi();
 
-app.listen("3000", () => {
-  console.log("Server is running on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
